835896864c0bf417a5bc8e11275153b598190e21
polyakale <polyakalex52@gmail.com>
2024-09-06 11:16:58 +0200

    ok

--- src/views/Alex.vue
+++ src/views/Alex.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Alex vagyok</h1>
+    <div>
+        <h1>Alex vagyok</h1>
+        <p>A 2001. szeptember 11-ei terrortámadások (gyakori rövidítése: 9/11, ami a szeptember 11-ei dátum amerikai írásmódja; kimondva: szeptember tizenegy) az al-Káida terrorszervezet összehangolt öngyilkos merényletsorozata volt az Egyesült Államok ellen.</p>
+
+    </div>
 </template>
 
 <script>
