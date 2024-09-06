67bbf5682956bc4e58829625ee8bdeeee7537b4b
HajduIstvan02 <hajduistvan@ugmszi.hu>
2024-09-06 11:51:12 +0200

    istvan vue kész

--- src/views/Istvan.vue
+++ src/views/Istvan.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>István vagyok</h1>
+        <p>Hajdu István vagyok egy basszeros több dolgot nem osztok meg. Kész vég a mesének..... Ennyi... nincs több mesélés</p>
     </div>
 </template>
 
