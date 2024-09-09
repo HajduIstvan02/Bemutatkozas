5ccf426f813e08eab37139aeba2f026b6887af5a
polyakale <polyakalex52@gmail.com>
2024-09-09 14:31:21 +0200

    fix

--- src/views/Alex.vue
+++ src/views/Alex.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container container-fluid mine-background">
+    <div class="container container-fluid mine-background mine-border">
         <h1>Alex vagyok</h1>
         <p>Repülő rovar, amelyek közeli rokonságban áll a darazsakkal és a hangyákkal. Külön vonalat képvisel a méhalkatúak (Apoidea) főcsaládban, jelenleg az Anthophila kládba sorolják. Megközelítőleg 20 000 ismert faja létezik, habár sokuk nincsen leírva, illetve besorolva. Gazdasági jelentőségű, mézet termelő rovar, szerepe a növények beporzásában nélkülözhetetlen.</p>
         <img src="/public/meh.png" alt="T">
@@ -14,7 +14,9 @@ export default {
 
 <style>
 .mine-background {
-  background-color: rgb(127, 195, 240);
+  background-color: rgb(251, 255, 0);
 }
-
+.mine-border {
+  border: 2px solid rgb(27, 0, 182);
+}
 </style>
