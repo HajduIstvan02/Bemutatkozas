9a2df4edf874bc15b1cdb805c4c1f800eebfcfbf
HajduIstvan02 <hajduistvan@ugmszi.hu>
2024-09-10 09:17:10 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
           </p>
         </nav>
       </div>
-      <div class="containre container-fluid my-container my-border my-background position-middle">
-        <h1>Rólunk</h1>
-        <p>Sziasztok ezen az oldalon fogunk bemutatkozni. Ammyit kell tudni rólunk hogy nem az erősségünk az objektum orientált programozás. Számunkra kényelmesebb a Phyton programozás ami parancsvezérelt.</p>
-        <h6>(esetleges hajtépésért a borzalmas dizájn és kódért elnézésüket kérjük)</h6>
-        
-      </div>
+     
     </header>
     
     <RouterView/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,4 +6,10 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
     <TheWelcome/>
   </main>
+  <div class="containre container-fluid my-container my-border my-background position-middle">
+        <h1>Rólunk</h1>
+        <p>Sziasztok ezen az oldalon fogunk bemutatkozni. Ammyit kell tudni rólunk hogy nem az erősségünk az objektum orientált programozás. Számunkra kényelmesebb a Phyton programozás ami parancsvezérelt.</p>
+        <h6>(esetleges hajtépésért a borzalmas dizájn és kódért elnézésüket kérjük)</h6>
+        
+      </div>
 </template>
