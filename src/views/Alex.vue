fafd2b5efa1c5d45f92168f0e16521171a79c446
polyakale <polyakalex52@gmail.com>
2024-09-10 08:43:09 +0200

    no meh

--- src/views/Alex.vue
+++ src/views/Alex.vue
@@ -3,7 +3,6 @@
       <img src="/public/fedy.png" alt="Fedy">
       <h1>Alex vagyok</h1>
         <p>Repülő rovar, amelyek közeli rokonságban áll a darazsakkal és a hangyákkal. Külön vonalat képvisel a méhalkatúak (Apoidea) főcsaládban, jelenleg az Anthophila kládba sorolják. Megközelítőleg 20 000 ismert faja létezik, habár sokuk nincsen leírva, illetve besorolva. Gazdasági jelentőségű, mézet termelő rovar, szerepe a növények beporzásában nélkülözhetetlen.</p>
-        <img src="/public/meh.png" alt="Meh">
     </div>
 </template>
 
